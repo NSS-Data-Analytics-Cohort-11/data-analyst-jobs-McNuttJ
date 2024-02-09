c1493ab5ab6ce6c4498b83374fd0ac41528808bf
McNuttJ <mcnuttj34@gmail.com>
2024-02-08 19:44:07 -0600

    finished question 6

--- scripts/data_analyst_jobs_scripts.sql
+++ scripts/data_analyst_jobs_scripts.sql
@@ -36,3 +36,15 @@ SELECT COUNT(review_count)
 FROM data_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000;
 --Answer: 151
+
+--Questions 6. Show the average star rating for companies in each state. The output should show the state as 'state' and the average rating for the state as 'avg_rating'. Which state shows the highest average rating?
+SELECT AVG(star_rating) AS avg_rating, location AS state
+FROM data_analyst_jobs
+GROUP BY location
+ORDER BY avg_rating DESC;
+--Answer: NE
+
+
+
+
+
